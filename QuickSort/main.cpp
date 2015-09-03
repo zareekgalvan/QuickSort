2ac456397010889deac2b57b8bfe35e0574771da
Armando Galván <zareek99@gmail.com>
2015-09-03 12:48:36 -0500

    Initial Commit

---
+++ QuickSort/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  QuickSort
+//
+//  Created by Zareek Galvan on 9/3/15.
+//  Copyright (c) 2015 Trisquel Labs. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
